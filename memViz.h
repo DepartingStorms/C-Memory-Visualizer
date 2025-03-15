e606abcf109133b454cd47f40816b2aacbcfefe0
DepartingStorms <departingstorms@gmail.com>
2025-03-15 01:48:48 -0400

    Create memViz.h

---
+++ memViz.h
@@ -0,0 +1,8 @@
+#ifndef MEMVIZ_H INCLUDED
+#define MEMVIZ_H INCLUDED
+
+void visualizeMemory(void* ptr, size_t size);
+void visualizeMemoryLayout();
+void array_visualization();
+
+#endif MEMVIZ_H INCLUDED
