d0b43fa57b79d81c98f4593d36941828bda8414b
DepartingStorms <departingstorms@gmail.com>
2025-03-15 01:37:53 -0400

    Added main.c code from original local file

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
+    // Demo with various data types
+    int i = 0;
+    float f = 0.0001;
+    char c = 'A';
+    double d = 0.00001;
+
+    printf("===== INTEGER =====\n");
+    visualizeMemory(&i, sizeof(i));
+
+    printf("===== FLOAT =====\n");
+    visualizeMemory(&f, sizeof(f));
+
+    printf("===== CHAR =====\n");
+    visualizeMemory(&c, sizeof(c));
+
+    printf("===== DOUBLE =====\n");
+    visualizeMemory(&d, sizeof(d));
+
+    visualizeMemoryLayout();
+    array_visualization();
+
+
+    return 0;
+}
